c70846c1df40ae5527e0389cdda5d2b6b4701172
Blaž Stojanovič <bs673@cam.ac.uk>
2021-06-08 22:17:46 +0100

    restructuring of folder

---
+++ qsampling/potential.py
@@ -0,0 +1,51 @@
+"""
+Potentials.py
+
+potential functions for finite lattice models
+
+"""
+
+import jax.numpy as jnp
+
+
+def ising_potential():
+	""" 
+	Potential for square Ising model with p.b.c.
+	
+	"""
+
+	def potential():
+			pass
+
+
+	return potential
+
+
+
+
+
+
+
+def xy_potential():
+	""" 
+	Potential for square Ising model with p.b.c.
+	
+	"""
+
+	def potential():
+		pass
+
+
+	return potential
+
+def bosehubbard_potential():
+	""" 
+	Potential for square Ising model with p.b.c.
+	
+	"""
+
+	def potential():
+		pass
+
+
+	return potential
